3178fba6995b5c47191d5fb34d2c82cce98973ac
TSENG PO-JUI <ff9418290@gmail.com>
2025-12-03 00:11:45 +0800

    新增matrix.h

---
+++ matrix.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#define SIZE 3
+
+void mat_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZE]);
+void mat_transpose(double A[3][3], double T[3][3]);
+
+#endif
