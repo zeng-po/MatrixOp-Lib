fe9123695bc56957d5cb3324635e881641de61ab
TSENG PO-JUI <ff9418290@gmail.com>
2025-12-03 21:06:31 +0800

    修改basic內容、新增basic測試

--- basic.h
+++ basic.h
@@ -1,8 +1,13 @@
+/*
+加法 : VADD(矩陣A, 矩陣B)
+減法 : VSUB(矩陣A, 矩陣B)
+Hadamard乘法 : VDOT(矩陣A, 矩陣B)
+*/
 #ifndef BASIC_H
 #define BASIC_H
 
-int* VADD(int *A,int *B,int size);
-int* VSUB(int *A,int *B,int size);
-int* VDOT(int *A,int *B,int size);
+int** VADD(int A[3][3], int B[3][3]);
+int** VSUB(int A[3][3], int B[3][3]);
+int** VDOT(int A[3][3], int B[3][3]);
 
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
+#include"basic.h"
+/*
+加法 : VADD(矩陣A, 矩陣B)
+減法 : VSUB(矩陣A, 矩陣B)
+Hadamard乘法 : VDOT(矩陣A, 矩陣B)
+*/
 
 int main ()
 {
+    int A[3][3]={{1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}};
+    int B[3][3]={{9, 6, 3},
+                        {8, 5, 2},
+                        {7, 4, 1}};
+    int** C;
+    //C = VADD(A, B);
+    for(int i= 0 ; i<3 ; i++)
+    {
+        for(int j = 0 ; j < 3 ; j++)
+            printf("%3d", C[i][j]);
+        printf("\n");
+    }
     return 0;
 }
