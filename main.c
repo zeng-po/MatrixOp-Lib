e69dcf478d755c8ef2e9b890a24d946b9b9ccf36
TSENG PO-JUI <ff9418290@gmail.com>
2025-12-07 22:11:59 +0800

    invers引入原有函數

--- main.c
+++ main.c
@@ -2,29 +2,30 @@
 #include"basic.h"
 #include"advanced.h"
 #include"linear.h"
+#include"inverse.h"
 
 /*
 加法 : VADD(矩陣A, 矩陣B)
 減法 : VSUB(矩陣A, 矩陣B)
 Hadamard乘法 : VDOT(矩陣A, 矩陣B)
-*/
-/*
+
 矩陣相乘 : mat_multiply(矩陣 A , 矩陣 B )
 轉置矩陣 : mat_transpose(矩陣 A )
-*/
-/*
+
 伴隨矩陣 : adjoint(矩陣A)
 行列式值 : determinant(矩陣A)
+
+反矩陣 : mat_inverse(矩陣A)
 */
 
 int main ()
 {
     double A[3][3]={{1, 2, 3},
-                    {4, 5, 6},
-                    {7, 8, 9}};
+                               {4, 5, 6},
+                               {7, 8, 8}};
     double B[3][3]={{9, 6, 3},
-                    {8, 5, 2},
-                    {7, 4, 1}};
+                               {8, 5, 2},
+                               {7, 4, 1}};
     double** C;
     double x;
     //----測試區
@@ -36,16 +37,17 @@ int main ()
     //C = mat_transpose( A );
 
     //C = adjoint(A);
-    x = determinant(A);
+    //x = determinant(A);
+
+    C = mat_inverse(A);
     //----測試區
-    /*
+
     for(int i= 0 ; i<3 ; i++)
     {
         for(int j = 0 ; j < 3 ; j++)
-            printf("%4.0f", C[i][j]);
+            printf("%4.2f ", C[i][j]);
         printf("\n");
     }
-    */
-    printf("%4.0f", x);
+
+    //printf("%4.0f", x);
     return 0;
-}
